1c2ab8c92347cb376df28a2ef6963ee378f7a12b
omar khedr <omar.khedr7172006@gmail.com>
2025-12-06 21:34:37 +0200

    trying

---
+++ draw.c
@@ -0,0 +1,40 @@
+void draw(char *board,char *kill_white,char *kill_black){
+    char col ='A';
+    int x = 8;
+    for(int i=0 ; i <8 ;i++){
+        if(i == 0){
+            printf("\n  ");
+            for(int n=0;n<8;n++){printf("  %c ",col+n);}
+            printf("    White Black\n  ");}
+        for(int k=0;k<8;k++){ printf("|---");}  
+        printf("|     %s     %s\n%d ",kill_white[i],kill_black[i],x-i);
+        for(int j=0;j<8;j++){ printf("| %s ",board[i][j]);}  
+        printf("| %d   %s     %s\n  ",x-i,kill_white[i+8],kill_black[i+8]);
+            if (i==7){
+                for(int k=0;k<8;k++){printf("|---");}
+                printf("|\n  ");
+                for(int n=0;n<8;n++){printf("  %c ",col+n);}
+                printf("\n");}
+        }
+}
+#include<stdio.h>
+int main(){
+    char board[8][8][32] = {
+        {"♖", "♘", "♗", "♔", "♕", "♗", "♘", "♖"},
+        {"♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {"♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"},
+        {"♜", "♞", "♝", "♚", "♛", "♝", "♞", "♜"}
+        
+        
+    };
+    char kill_white[16][32]={"♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟",
+                            "♜", "♞", "♝", "♚", "♛", "♝", "♞", "♜"};
+    char kill_black[16][32]={"♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙",
+                        "♖", "♘", "♗", "♔", "♕", "♗", "♘", "♖"};
+        draw(board,kill_white,kill_black);
+        return 0;
+}
